3ef40361b25d6836a28fdaf575ba194f526438f0
0441106 <0441106@nkust.edu.tw>
2018-05-15 22:42:07 +0800

    change first file

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,i=0;
-	for(a=0;a<11;a++){
-		i=i+a;
+	int constant,variable=11,sum=0;
+	for(constant=0;constant<variable;constant++){
+		sum=sum+constant; //1 plus to variable(10)
 	}
-	printf("i=%d\n",i);
+	printf("sum=%d\n",sum); //print sum
 	return 0;
 }
